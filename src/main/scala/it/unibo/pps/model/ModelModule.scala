a98969e1e141336609ac4acbcea87c2b24d721ee
dom <davidedomini@gmail.com>
2022-07-19 18:42:00 +0200

    feat: implemented init method in model

--- src/main/scala/it/unibo/pps/model/ModelModule.scala
+++ src/main/scala/it/unibo/pps/model/ModelModule.scala
@@ -1,8 +1,11 @@
 package it.unibo.pps.model
 
+import it.unibo.pps.model.body.Body
+
 object ModelModule:
 
   trait Model:
+    def init(): Unit
     def addSnapshot(snapshot: Snapshot): Unit
     def getLastSnapshot(): Snapshot
 
@@ -12,6 +15,7 @@ object ModelModule:
   trait Component:
     class ModelImpl extends Model:
       private var history: List[Snapshot] = List.empty
+      override def init(): Unit = history :+ Snapshot(Body((10,10), (1,1)))
       override def addSnapshot(snapshot: Snapshot): Unit = history = history :+ snapshot
       override def getLastSnapshot(): Snapshot = history.last
 
