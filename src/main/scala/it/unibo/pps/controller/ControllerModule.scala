f10a1c20c9c7e15197b74ee5bab5028b767951f6
dom <davidedomini@gmail.com>
2022-07-19 19:04:20 +0200

    feat: implemented notifystart method in controller

--- src/main/scala/it/unibo/pps/controller/ControllerModule.scala
+++ src/main/scala/it/unibo/pps/controller/ControllerModule.scala
@@ -1,5 +1,9 @@
 package it.unibo.pps.controller
 
+import it.unibo.pps.engine.EngineModule
+import it.unibo.pps.model.ModelModule
+import monix.execution.Scheduler.Implicits.global
+
 object ControllerModule:
 
   trait Controller:
@@ -8,8 +12,17 @@ object ControllerModule:
   trait Provider:
     val controller: Controller
 
+  type Requirements = ModelModule.Provider with EngineModule.Provider
+  
   trait Component:
+    context: Requirements =>
     class ControllerImpl extends Controller:
-      override def notifyStart(): Unit = ???
+      override def notifyStart(): Unit =
+        context.model.init()
+        context.simulationEngine
+          .simulationStep()
+          .loopForever
+          .runAsyncAndForget
 
-  trait Interface extends Provider with Component
+  trait Interface extends Provider with Component:
+    self: Requirements =>
--- src/main/scala/it/unibo/pps/launcher/Launcher.scala
+++ src/main/scala/it/unibo/pps/launcher/Launcher.scala
@@ -3,15 +3,18 @@ package it.unibo.pps.launcher
 import it.unibo.pps.view.ViewModule
 import it.unibo.pps.model.ModelModule
 import it.unibo.pps.controller.ControllerModule
+import it.unibo.pps.engine.EngineModule
 
 object Launcher
   extends ControllerModule.Interface
   with ModelModule.Interface
-  with ViewModule.Interface:
+  with ViewModule.Interface
+  with EngineModule.Interface:
 
   override val controller = ControllerImpl()
   override val model = ModelImpl()
   override val view = ViewImpl()
+  override val simulationEngine = SimulationEngineImpl()
 
   @main def Main(): Unit =
     println("Starting simulation")
