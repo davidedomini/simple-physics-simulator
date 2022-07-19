95636f62dcbabbd3fbc7feaf609403bd0a204cb9
dom <davidedomini@gmail.com>
2022-07-19 18:34:32 +0200

    feat: implemented render method in view module

--- src/main/scala/it/unibo/pps/view/ViewModule.scala
+++ src/main/scala/it/unibo/pps/view/ViewModule.scala
@@ -1,15 +1,24 @@
 package it.unibo.pps.view
 
+import it.unibo.pps.controller.ControllerModule
+import it.unibo.pps.model.Snapshot
+
 object ViewModule:
 
   trait View:
-    def render(): Unit
+    def render(s: Snapshot): Unit
 
   trait Provider:
     val view: View
 
+  type Requirements = ControllerModule.Provider
+  
   trait Component:
+    context: Requirements =>
     class ViewImpl extends View:
-      override def render(): Unit = ???
+      private val gui = Gui(400, 200, context.controller)
+      
+      override def render(s: Snapshot): Unit = gui render s.body
 
-  trait Interface extends Provider with Component
+  trait Interface extends Provider with Component:
+    self: Requirements =>
