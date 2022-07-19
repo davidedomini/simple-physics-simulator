84277884f476326a374e3b7f4fbf294cbf2217f3
dom <davidedomini@gmail.com>
2022-07-19 17:35:55 +0200

    feat(RichTuple2): implemented methods for sum, module and normalization

--- src/main/scala/it/unibo/pps/util/RichTuple2.scala
+++ src/main/scala/it/unibo/pps/util/RichTuple2.scala
@@ -3,4 +3,7 @@ package it.unibo.pps.util
 object RichTuple2:
   extension(v: Tuple2[Double, Double])
     def x(n: Double): Tuple2[Double, Double] = (v._1 * n, v._2 * n)
+    def ++(v2: Tuple2[Double, Double]): Tuple2[Double, Double] = (v._1 + v2._1, v._2 + v2._2)
+    def module: Double = Math.sqrt(v._1 * v._1 + v._2 * v._2)
+    def normalize: Tuple2[Double, Double] = (v._1/v.module, v._2/v.module)
 
