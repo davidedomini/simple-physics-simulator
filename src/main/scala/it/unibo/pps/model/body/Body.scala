b4e1950e41b4dcf6cb9a9986ab099a4f11d1d7c7
dom <davidedomini@gmail.com>
2022-07-19 17:50:25 +0200

    feat(Body): implemented methods for computing new velocity and new position

--- src/main/scala/it/unibo/pps/model/body/Body.scala
+++ src/main/scala/it/unibo/pps/model/body/Body.scala
@@ -1,5 +1,13 @@
 package it.unibo.pps.model.body
 
+import it.unibo.pps.util.RichTuple2.*
+
 case class Body(position: Tuple2[Double, Double], velocity: Tuple2[Double, Double])
 
 object Body:
+  def computeNewVelocity(acceleration: Tuple2[Double, Double], oldVelocity: Tuple2[Double, Double], dt: Double) =
+    oldVelocity ++ (acceleration x dt)
+
+  def computeNewPosition(velocity: Tuple2[Double, Double], oldPosition: Tuple2[Double, Double], dt: Double) =
+    oldPosition ++ (velocity x dt)
+
